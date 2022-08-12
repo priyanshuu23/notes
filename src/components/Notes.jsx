d5612197301087a12e62b339b7a328763481e180
priyanshu urkade <96242630+priyanshuu23@users.noreply.github.com>
2022-08-12 10:06:26 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> This is a note</h1>
+      <p>This project is created by Priyanshu Urkade </p>
+    </div>
+  );
+}
+
+export default Notes;
